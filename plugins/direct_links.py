6076cf02449eeab05a2db48e3925dde3ae0c068c
oscillot <alt.fractal.enthogenics@gmail.com>
2012-12-29 13:01:08 -0600

    Converted default direct link handler to a stand alone plugin

---
+++ plugins/direct_links.py
@@ -0,0 +1,11 @@
+def execute(children, candidates):
+    handled = []
+    for child in children:
+        for img_type in ['.jpg', '.jpeg', '.gif', '.bmp', '.png']:
+            if child['data']['url'].lower().endswith(img_type):
+                candidates.append({'url' : child['data']['url'],
+                                   'subreddit' : child['data']['subreddit'],
+                                   'title' : child['data']['title']})
+            handled.append(child)
+            break
+    return handled, candidates
