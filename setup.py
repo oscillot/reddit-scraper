e6f57a4d05b2c4c2e16aa9493cf1d6cf8819bcd9
oscillot <alt.fractal.enthogenics@gmail.com>
2013-02-11 20:54:25 -0600

    Setup.py works, egg stuff added to gitignore

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='reddit-scraper',
     version='1.1',
-    requires=['pillow>=1.7.8', 'lxml>=3.0.2'],
+    packages=find_packages(),
+    install_requires=['pillow>=1.7.8', 'lxml>=3.0.2'],
     url='https://github.com/oscillot/reddit-scraper',
     license='GPL2',
     author='Oscillot',
