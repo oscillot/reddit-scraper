8315c82d96a62c747310713a25306c5e406b377f
oscillot <alt.fractal.enthogenics@gmail.com>
2013-02-13 21:24:10 -0600

    Fixed the issue where wallbase got progressively slower and then bombed out toally

--- plugins/wallbase_collection.py
+++ plugins/wallbase_collection.py
@@ -5,6 +5,7 @@ import urllib2
 from lxml import etree
 import re
 import base64
+import time
 
 def wget(url):
     resp = urllib.urlopen(url)
@@ -18,19 +19,21 @@ def get_wallbase_collection(url):
         print e
         return []
     tree = etree.HTML(resp.read())
-    thumb_links = []
     max_images = int(tree.find('.//*[@id="delwrap"]/div[1]/div[3]/span[1]').text)
     pages = []
-    start = 0
+    start = 1
     end = 32
     while end <= max_images + 64: #for some reason everything is shifted by
     # 32, plus we add an extra 32 for a full exceed above the required
-        pages.append('%s%d/%d' % (url, start, end))
+        p = '%s%d/%d' % (url, start, end)
+        pages.append(p)
         start += 32
         end += 32
     urls = []
-    for p in pages:
+    for i, p in enumerate(pages):
+        thumb_links = []
         tree = etree.HTML(wget(p).read())
+        t1 = time.time()
         for script in tree.findall('.//*[@class="thumb"]/a'):
             img_link = script.get('href')
             thumb_links.append(img_link)
@@ -54,7 +57,6 @@ def get_wallbase_collection(url):
 def execute(children, candidates):
     handled = []
     for child in children:
-        print child['data']['url']
         if child['data']['url'].lower().startswith('http://wallbase.cc/user/collection/'):
             collection_imgs = get_wallbase_collection(child['data']['url'])
             for img in collection_imgs:
