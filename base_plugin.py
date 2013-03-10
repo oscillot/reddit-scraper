620c7731c77bb468a510a722029e0aff6dcae4f3
oscillot <alt.fractal.enthogenics@gmail.com>
2013-03-10 00:15:08 -0800

    Add the base class for plugins

---
+++ base_plugin.py
@@ -0,0 +1,38 @@
+class BasePlugin():
+    def __init__(self, candidates):
+        """Constructs the plugin calls and executes them, saving successes,
+        failures and links as class attributes
+
+        :param list candidates: a list of dictionaries converted from the json
+        response given back by reddit.
+        :rtype list, list: a list of the dictionary data that was successfully
+        parsed by this plugin, a list of dictionaries with the url,
+        subreddit and title of the direct link for later acquisition and database
+         entry
+        """
+        self.candidates = candidates
+        self.to_acquire = []
+        self.handled = []
+        self.exceptions = []
+        self.__execute()
+
+    def __execute(self):
+        """
+        This hidden executor handles iterating through the list of links and
+        catching any unexpected exceptions thrown by the plugin gracefully,
+        reporting the plugin, link and exception for output at the end of the
+         run
+        """
+        for candidate in self.candidates:
+            try:
+                self.execute(candidate)
+            except Exception, e:
+                self.exceptions.append((candidate, e, self.__class__.__name__))
+
+    def execute(self, candidate):
+        """
+        To be overridden by subclasses. The subclassed versions of this
+        method should be written to handle just one post link,
+        but can call other functions as needed.
+        """
+        pass
