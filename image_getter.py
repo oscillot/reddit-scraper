340c2299cb33e4d3fdfe924279009b00d35fcb0a
sernatin <paul.sernatinger@nokia.com>
2013-11-04 13:33:07 -0600

    change .text to .content (might be mroe of these...)

--- image_getter.py
+++ image_getter.py
@@ -330,14 +330,14 @@ class ImageGetter():
                                              candidate['url']))
                 self.handle_exception(e, candidate)
                 continue
-
-            if resp.headers.get('content-encoding') == 'gzip':
-                gzipped_img = resp.text
-                img_data = gzip.GzipFile(fileobj=StringIO(gzipped_img))
-                new_img = img_data.read()
-                img_data.close()
+            #this is handled by requests now
+            #if resp.headers.get('content-encoding') == 'gzip':
+            #    gzipped_img = resp.text
+            #    img_data = gzip.GzipFile(fileobj=StringIO(gzipped_img))
+            #    new_img = img_data.read()
+            #    img_data.close()
             else:
-                new_img = resp.text
+                new_img = resp.content
             md5 = hashlib.md5(new_img).hexdigest()
             if md5 not in unique_img_hashes:
                 img_path = os.path.join(self.output, candidate['filename'])
